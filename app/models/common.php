955fdf4968d7818f458ea7607e6dce7e7bc1656f
Markus Gray <markusgray@syncwaretech.com>
2014-10-07 09:11:10 -0400

    Finished up user authentication methods

--- app/models/common.php
+++ app/models/common.php
@@ -7,4 +7,12 @@ class Common extends CI_Model{
 	function isAdmin(){
 		
 	}
+
+	function email_unique($email){
+
+	}
+
+	function username_unique($username){
+		
+	}
 }
--- app/models/users_model.php
+++ app/models/users_model.php
@@ -1,4 +1,60 @@
 <?php
 class Users_Model extends MY_Model{
 	protected $table = "users";
+
+	function signup($name, $email, $password){
+		$salt = md5(time());
+		$Encryptedpassword = sha1($salt.$password);
+
+		if($this->common->unique_email($email)){
+			$newUser = array(
+				"name" => $name,
+				"email" => $email,
+				"salt" => $salt,
+				"password" => $Encryptedpassword,
+				"created" => time()
+			);
+
+			$id = $this->db->insert($this->table, $newUser);
+
+			if($id){
+				$this->session->set_userdata("uid", $id);
+				redirect(site_url());
+			}
+			else{
+				$this->session->set_flashdata("error", "Unable to signup at this time. Please try again");
+				redirect(site_url('signup'));
+			}
+		}
+		else{
+			$this->session->set_flashdata("error", "This email address is already registered");
+			redirect(site_url('signup'));
+		}
+	}
+
+
+	function login($email, $password){
+		$this->db->where("email", $email);
+		$user = $this->db->get($this->table)->row_array();
+
+		if($user){
+			if($user['password'] == sha1($user['salt'].$password)){
+				$this->session->set_userdata("uid", $user['id']);
+				redirect(site_url());
+			}
+			else{
+				$this->session->set_flashdata("error", "You have entered an incorrect email and password combo");
+				redirect(site_url('signup'));
+			}
+		}
+		else{
+			$this->session->set_flashdata("error", "This email address is not registered");
+			redirect(site_url('login'));
+		}
+	}
+
+	function logout(){
+		$this->session->sess_destroy();
+		redirect(site_url());
+	}
 }
