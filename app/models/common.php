0d5d9335442d210dc16af657bb3f2ba4c135337f
Markus Gray <m.gray@thetactilegroup.com>
2014-10-09 11:59:10 -0400

    Fill out common input

--- app/models/common.php
+++ app/models/common.php
@@ -1,18 +1,41 @@
 <?php
 class Common extends CI_Model{
 	function loggedin(){
-
+		$id = $this->session->userdata("uid");
+		if($id){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	function isAdmin(){
-		
+		$id = $this->session->userdata("uid");
+
+		$user = $this->db->where(array(
+			"id" => $id,
+			"admin" => 1
+		))->count_all_results("users");
 	}
 
 	function email_unique($email){
-
+		$user = $this->db->where("email",$email)->count_all_results("users");
+		if($user){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 
 	function username_unique($username){
-		
+		$user = $this->db->where("username",$username)->count_all_results("users");
+		if($user){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
 }
